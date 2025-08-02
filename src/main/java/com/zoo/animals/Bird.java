18dd4fd765428e4cb211ca1f62269ddc46c3044d
mrs-liliia-kutova <lilqatester@gmail.com>
2025-08-02 20:18:53 +0200

    Update Bird.java

--- src/main/java/com/zoo/animals/Bird.java
+++ src/main/java/com/zoo/animals/Bird.java
@@ -1,4 +1,24 @@
 package com.zoo.animals;
 
-public class Bird {
+public class Bird extends Animal {
+    protected String wingSpan;
+
+    public Bird(String name, int age, int weight, int energyLevel) {
+        super(name, age, weight, energyLevel);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + "makes a sound! ");
+    }
+
+    public void fly(){
+        int currentEnergy = getEnergyLevel();
+        currentEnergy -=15;
+        setEnergyLevel(currentEnergy);
+    }
+
+    protected void setEnergyLevel(int currentEnergy) {
+
+    }
 }
