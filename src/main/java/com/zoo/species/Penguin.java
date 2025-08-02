e238367b39d0045afa0d12d087825d2b553934d1
mrs-liliia-kutova <lilqatester@gmail.com>
2025-08-02 21:13:38 +0200

    Update Penguin.java

--- src/main/java/com/zoo/species/Penguin.java
+++ src/main/java/com/zoo/species/Penguin.java
@@ -12,7 +12,11 @@ public class Penguin extends Bird {
         System.out.println("Кря-кря! ");
     }
     public void swim(){
+        int currentEnergy =getEnergyLevel();
+        currentEnergy -=20;
+        setEnergyLevel(currentEnergy);
         System.out.println(" Пінгвін не літає, але плаває" );
+
     }
 
     @Override
