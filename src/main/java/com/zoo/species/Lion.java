014b7e1b84f00915bc491adca2845503b4a25412
mrs-liliia-kutova <lilqatester@gmail.com>
2025-08-02 20:19:07 +0200

    Update Lion.java

--- src/main/java/com/zoo/species/Lion.java
+++ src/main/java/com/zoo/species/Lion.java
@@ -1,4 +1,25 @@
 package com.zoo.species;
 
-public class Lion {
+import com.zoo.animals.Mammal;
+
+public class Lion extends Mammal {
+    public Lion(String name, int age, int weight, int energyLevel) {
+        super(name, age, weight, energyLevel);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Ppppp! ");
+    }
+
+public void hunt(){
+    int currentEnergy =getEnergyLevel();
+    currentEnergy -=25;
+    setEnergyLevel(currentEnergy);
+}
+
+    private void setEnergyLevel(int currentEnergy) {
+
+    }
+
 }
