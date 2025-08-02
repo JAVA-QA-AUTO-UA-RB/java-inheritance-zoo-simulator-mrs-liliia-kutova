82410e94bcf7d2c1bdadc4d609dc930e4cf4d83e
mrs-liliia-kutova <lilqatester@gmail.com>
2025-08-02 20:19:10 +0200

    Update Penguin.java

--- src/main/java/com/zoo/species/Penguin.java
+++ src/main/java/com/zoo/species/Penguin.java
@@ -1,4 +1,22 @@
 package com.zoo.species;
 
-public class Penguin {
+import com.zoo.animals.Bird;
+
+public class Penguin extends Bird {
+    public Penguin(String name, int age, int weight, int energyLevel) {
+        super(name, age, weight, energyLevel);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Кря-кря! ");
+    }
+    public void swim(){
+        System.out.println(" Пінгвін не літає, але плаває" );
+    }
+
+    @Override
+    public void fly(){
+        swim();
+    }
 }
