d194b6bed4b2d21472e3da80ba136eee1fea77f9
mrs-liliia-kutova <lilqatester@gmail.com>
2025-08-02 20:19:04 +0200

    Update Elephant.java

--- src/main/java/com/zoo/species/Elephant.java
+++ src/main/java/com/zoo/species/Elephant.java
@@ -1,4 +1,16 @@
 package com.zoo.species;
 
-public class Elephant {
+import com.zoo.animals.Mammal;
+
+public class Elephant extends Mammal {
+    public Elephant(String name, int age, int weight, int energyLevel) {
+        super(name, age, weight, energyLevel);
+    }
+    @Override
+    public void makeSound() {
+        System.out.println("Труба-ба-ба! ");
+    }
+  public void spraySelf(){
+
+  }
 }
