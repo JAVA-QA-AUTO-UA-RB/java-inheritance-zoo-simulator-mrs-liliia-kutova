cf757bd00fa25e64c086f63a4cb088c339d57cdb
mrs-liliia-kutova <lilqatester@gmail.com>
2025-08-18 21:40:53 +0200

    Update Elephant.java

--- src/main/java/com/zoo/species/Elephant.java
+++ src/main/java/com/zoo/species/Elephant.java
@@ -1,15 +1,38 @@
 package com.zoo.species;
 
+import com.zoo.animals.Animal;
 import com.zoo.animals.Mammal;
+import com.zoo.interfaces.Playable;
 
-public class Elephant extends Mammal {
-    public Elephant(String name, int age, int weight, int energyLevel) {
-        super(name, age, weight, energyLevel);
+public class Elephant extends Mammal implements Animal.Eatable, Animal.Sleepable, Playable {
+    public Elephant(String name, int energyLevel) {
+        super(name, energyLevel);
     }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
     @Override
     public void makeSound() {
         System.out.println("Труба-ба-ба! ");
     }
+
+    @Override
+    public void move() { System.out.println(getName() + " сильно тупотить");
+    }
+
+    @Override
+    public void play(){
+        System.out.println(getName() + " грається ");
+    }
+
   public void spraySelf(){
       int currentEnergy =getEnergyLevel();
       currentEnergy -=15;
