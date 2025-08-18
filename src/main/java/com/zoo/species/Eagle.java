c8d2805bc5278457c490fb3975b6290067f3bf81
mrs-liliia-kutova <lilqatester@gmail.com>
2025-08-18 21:40:48 +0200

    Update Eagle.java

--- src/main/java/com/zoo/species/Eagle.java
+++ src/main/java/com/zoo/species/Eagle.java
@@ -1,11 +1,30 @@
 package com.zoo.species;
 
+import com.zoo.animals.Animal;
 import com.zoo.animals.Bird;
+import com.zoo.interfaces.Playable;
 
-public class Eagle extends Bird {
-    public Eagle(String name, int age, int weight, int energyLevel) {
-        super(name, age, weight, energyLevel);
+
+public class Eagle extends Bird implements Animal.Eatable, Animal.Sleepable, Playable {
+    public Eagle(String name, int energyLevel) {
+        super(name, energyLevel);
     }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void play(){
+        System.out.println(getName() + " грається ");
+    }
+
 
     @Override
     public void makeSound() {
@@ -16,6 +35,15 @@ public class Eagle extends Bird {
         int currentEnergy =getEnergyLevel();
         currentEnergy -=20;
         setEnergyLevel(currentEnergy);
+    }
+    private void buildNest(){
+        System.out.println(getName() + " побудував гніздо ");
+    };
+
+    @Override
+    public void uniqueBirdAction() {
+        buildNest();
+
     }
 
 }
